50236c4ee792e0efa3ebefd1bdd8ee798f06376a
unknown <sanjos@sanjos-PC.(none)>
2012-06-05 13:01:45 -0300

    Criando as primeiras interfaces

--- src/main/java/apresentacao/Principal.java
+++ src/main/java/apresentacao/Principal.java
@@ -1,43 +1,74 @@
 package apresentacao;
 
+import interector.ConsultarUsuarios;
+import interector.CriarUsuario;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+
+import negocio.Usuario;
 
 public class Principal {
+	
+	private Usuario usuario;
 
 	private void exibirMenu() {
-		System.out.println("1 - Criar usuário \n");
-		System.out.println("2 - Listar usuários \n");
-		System.out.println("3 - Adicionar em contatos \n");
-		System.out.println("4 - Criar evento \n");
-		System.out.println("s - Sair \n");
+		System.out.println("1 - Criar usuário");
+		System.out.println("2 - Listar usuários");
+		System.out.println("3 - Adicionar em contatos");
+		System.out.println("4 - Criar evento");
+		System.out.println("s - Sair");
 	}
 	
 	private void criaUsuario() {
-		
+		System.out.println("Digite o nome do usuário:");
+		String nome = readLine();
+		this.usuario = CriarUsuario.criarUsuario(nome);
+		System.out.println("Usuário criado com sucesso!");
 	}
 	
-    private static String readLine(InputStream in) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-        return reader.readLine();
-    }
+	private void listarUsuarios() {
+		LinkedList<String> nomes = ConsultarUsuarios.requisitar();
+		System.out.print(usuario.getNome());
+		for (String nome : nomes) {
+			System.out.print(", " + nome);
+		}
+	}
 	
-	public static void main(String args[]) {
-		
-		Principal main = new Principal();
-		main.exibirMenu();
-		try {
-			String entrada = readLine(System.in);
-			
-			switch (entrada.charAt(0)) {
-				case 1:
-					main.criaUsuario();
-					break;
-			}
+    private static String readLine() {
+    	InputStream in = System.in;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        try {
+			return reader.readLine();
 		} catch (IOException e) {
 			e.printStackTrace();
+			return "";
 		}
+    }
+	
+    private static String menuDeOpcoes() {
+    	Principal main = new Principal();
+    	main.exibirMenu();
+    	String entrada = readLine();
+    	System.out.println(entrada + " -> " + entrada.charAt(0));
+    	switch (new Integer(entrada)) {
+    	case 1:
+    		main.criaUsuario();
+    		break;
+    	case 2:
+    		main.listarUsuarios();
+    		break;			
+    	}
+    	return entrada;
+    }
+    
+	public static void main(String args[]) {
+		String entrada;
+		do {
+			entrada = menuDeOpcoes();
+		} while (! entrada.equalsIgnoreCase("s"));
 	}
 }
